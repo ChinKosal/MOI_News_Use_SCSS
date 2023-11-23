0bf6cfaeabb7d9a5c9f30f73422dac6c9d699ef5
Chin Kosal <nangapple82@gmail.com>
2023-11-23 11:49:26 +0700

    change folder

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -23,7 +23,7 @@
                     <a class="menu-hover last_text_item  text-white" onClick="">
                         ផ្សេងៗ
                     </a> 
-                    <span class="absolute top-5 right-0">
+                    <span class="absolute top-4.5 right-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 1024 1024"><path fill="currentColor" d="M104.704 338.752a64 64 0 0 1 90.496 0l316.8 316.8l316.8-316.8a64 64 0 0 1 90.496 90.496L557.248 791.296a64 64 0 0 1-90.496 0L104.704 429.248a64 64 0 0 1 0-90.496z"/></svg>
                     </span>
                     <div class="invisible absolute group-hover:visible  box_item_drop">
