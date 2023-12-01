02a7535ff3bcf70c0631578736b13cd15ead32ef
Chin Kosal <nangapple82@gmail.com>
2023-12-01 10:55:43 +0700

    update 1/12/2023

--- resources/views/components/section_1.blade.php
+++ resources/views/components/section_1.blade.php
@@ -182,4 +182,40 @@
     </div>
 </section>
 
+<script>
+    // slider js
+// Get the button element
+// Wait for the DOM to be ready
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialize Swiper
+    var swiper = new Swiper(".swiper-container", {
+        direction: "vertical",
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        effect: "slide", // Use the slide effect
+        speed: 1000, // Set the transition speed in milliseconds
+    });
+
+    // document.getElementById("nextButton").addEventListener("click", function () {
+    //   swiper.slideNext();
+    // });
+
+    document
+        .getElementById("scrollToTopButton1")
+        .addEventListener("click", function () {
+            swiper.slidePrev();
+        });
+});
+
+</script>
+
 
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   @vite(["resources/scss/app.scss","resources/js/app.js"])
 
